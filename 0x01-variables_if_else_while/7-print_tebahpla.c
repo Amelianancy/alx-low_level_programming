47f1c322cca929a8bbb6344c65259622e6e31601
Amelianancy <ogbonnanancy11@gmail.com>
2022-09-11 16:27:44 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: backward lower case
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch++)
+	{
+		putchar(number);
+	}
+putchar('\n');
+return (0);
+}
