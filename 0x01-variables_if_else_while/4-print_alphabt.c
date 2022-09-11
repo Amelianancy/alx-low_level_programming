c7bc543fa646bef865afd8dca23aeb70bcb431f6
Amelianancy <ogbonnanancy11@gmail.com>
2022-09-11 07:37:35 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: lower case
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char start;
+
+	for (start = 'a'; start <= 'z'; start++)
+{
+	if (start == 'e' || start == 'q')
+		continue;
+	else
+putchar(start);
+putchar('\n');
+return (0);
+}
