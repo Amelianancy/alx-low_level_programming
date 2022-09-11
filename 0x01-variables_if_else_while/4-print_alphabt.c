1d29b282a99f63a3a92720a4dfb58924857918ee
Amelianancy <ogbonnanancy11@gmail.com>
2022-09-11 15:28:49 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,12 @@ int main(void)
 	char start;
 
 	for (start = 'a'; start <= 'z'; start++)
-{
+	{
 	if (start == 'e' || start == 'q')
 		continue;
 	else
-putchar(start);
+	}  putchar(start);
 putchar('\n');
 return (0);
+
 }
