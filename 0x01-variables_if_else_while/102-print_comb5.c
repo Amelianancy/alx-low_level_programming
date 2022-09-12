ddd11ed98f79bd2efbe8ccc50e1ff3a8b9e243e8
Amelianancy <ogbonnanancy11@gmail.com>
2022-09-12 14:23:16 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 /**
 *main - entry point
-*Return: 0 (successfull)
+*Return: 0 (successful)
 */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
-
-	while (c <= 99)
-	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-
-		if (f_d < l_d)
-		{
-			putchar(f_d);
-			putchar(l_d);
-			if (c != 89)
-			{
-			putchar(',');
-			putchar(' ');
-			}
-		}
-		c++;
-	}
+int d1, d2, d3, d4, n = 0;
+for (d1 = '0'; d1 <= '9'; d1++)
+{
+for (d2 = '0'; d2 <= '9'; d2++)
+{
+for (d3 = '0'; d3 <= '9'; d3++)
+{
+for (d4 = d2 + 1; d4 <= '9'; d4++, n++)
+{
+if (n > 0)
+{
+putchar(',');
+putchar(' ');
+}
+putchar(d1);
+putchar(d2);
+putchar(' ');
+putchar(d3);
+putchar(d4);
+}
+}
+}
+}
 putchar('\n');
 return (0);
 }
