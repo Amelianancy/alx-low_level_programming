5790183ee7a7fe22f64db655ece0cb0c0e7eb29b
Amelianancy <ogbonnanancy11@gmail.com>
2022-09-13 05:41:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,10 +6,12 @@
 */
 int main(void)
 {
-char str[] = "_putchar";
-int ch;
-for (ch = 0; ch < 8; ++ch)
-_putchar(str[ch]);
+char *sh = "main";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
 _putchar('\n');
 return (0);
 }
