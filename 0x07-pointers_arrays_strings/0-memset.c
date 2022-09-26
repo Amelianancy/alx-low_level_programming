44587fa9982ae313350f7fda0f6d51a237b84a57
Amelianancy <ogbonnanancy11@gmail.com>
2022-09-26 15:42:10 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /*
-*_memset - Fills a buffer with a constant for a specified size
-* @s: This is the buffer to be filled
-*@b: This is the char that will be used to fill buffer
-*@n: This is the no of bytes to fill in the memory array of s
-*Return: a pointer to s memory area
+ ** _memset - fills memory with a constant byte
+ * @s: This is the buffer to be filled
+ * @b: This is the char that will be used to fill buffer
+ * @n: This is the no of bytes to fill in the memory array of s
+ * Return: a pointer to s memory area
 */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
