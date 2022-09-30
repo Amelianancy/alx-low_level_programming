d89db05bcd95a5449a5e9ca559228f71df950e9d
Amelianancy <ogbonnanancy11@gmail.com>
2022-09-30 04:49:42 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main"
+
+/**
+ *main - printf program name
+ *@argc: no of arguments
+ *@argv: array of string parameters
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+0x0A. C - argc, argv
+Argument count and Argument vector
+
+The declaration of main looks like this:
+int main(int argc, char *argv[]);
