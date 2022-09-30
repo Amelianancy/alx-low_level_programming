3789929935a93ed667d4203806362fe611cbec3a
Amelianancy <ogbonnanancy11@gmail.com>
2022-09-30 05:14:52 -0700

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,7 @@
  *@argv: array of string parameters
  *Return: 0
  */
-
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv[argc - 1]);
 	return (0);
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - entry point
+ *@argc: number of paramethers
+ *@argv: string of parameters
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = 0;
+
+	while (a < argc)
+	{
+		printf("%s\n", argv[a]);
+		a++;
+	}
+return (0);
+}
