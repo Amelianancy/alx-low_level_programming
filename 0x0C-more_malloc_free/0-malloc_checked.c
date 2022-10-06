ff6cea061bd437b6247ef821430c36654baae327
Amelianancy <ogbonnanancy11@gmail.com>
2022-10-06 05:44:37 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * malloc_checked -	Allocates specified memory size
+ * malloc_checked - Allocates specified memory size
  * @b:Amount of memore to be allocated
  * Return:Pointer to the allocated memory
  * Exits with status value 98 if malloc fails
- * */
+ */
 
 void *malloc_checked(unsigned int b)
 {
