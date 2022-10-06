9f873ea9f655c5b8ac672c176a5e81022406ce18
Amelianancy <ogbonnanancy11@gmail.com>
2022-10-06 12:19:14 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,31 +1,40 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * string_nconcat - concatenate the first n bytes of a string to another
- * @str1: pointer to the first string
- * @str2: pointer to the second string
- * @size: size of str2 to be concatenated
- * Return: pointer to the concatenated string
+ *string_nconcat - Concats 2 strings up to a no of bytes
+ *@s1: The first string
+ *@s2: The second string
+ *@n: The cap
+ *Return: A pointer to a string
  */
 
-char *string_nconcat(char *str1, char *str2, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int dist1, dist2;
-	char *ptr;
+	char *newString;
+	unsigned int size1, size2, i, j;
 
-	if (str1 == NULL)
-	str1 = "";
-	if (str2 == NULL)
-	str2 = "";
-	dist1 = strlen(str1);
-	dist2 = strlen(str2) <= size ? strlen(str2) : size;
-	ptr = malloc(dist1 + dist2 + 1);
-	if (ptr == NULL)
-	return (NULL);
-	strcpy(ptr, str1);
-	strncat(ptr, str2, size);
-return (ptr);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+	s2 = "";
+	size1 = size2 = 0;
+	while (s1[size1] != '\0')
+		size1++;
+	while (s2[size2] != '\0')
+		size2++;
+	if (n > size2)
+		n = size2;
+	newString = malloc(size1 + n + 1);
+	if (newString == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		newString[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+	{
+		newString[i] = s2[j];
+	}
+	newString[i] = '\0';
+	return (newString);
 }
