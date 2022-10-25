6dfe31f7c0d0d021a4d5d20cdcc9d978cc6545a2
Amelianancy <ogbonnanancy11@gmail.com>
2022-10-25 06:40:39 -0700

    fix 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,13 +2,9 @@
 
 /**
  * delete_nodeint_at_index - deletes the node at index
- *
  * of a linked list.
- *
  * @head: head of a list.
- *
  * @index: index of the list where the node is
- *
  * deleted.
  *
  * Return: 1 if it succeeded, -1 if it failed.
@@ -21,6 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	listint_t *prev;
 	listint_t *next;
+
 	prev = *head;
 	if (index != 0)
 	{
